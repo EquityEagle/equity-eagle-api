f9ae82ddf1247a70c874f9e8c7ae2915e282495a
Rapheal Chizitere <www.rashyking05@gmail.com>
2023-10-10 20:29:19 +0100

    fixed payload issues

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import SetupRoute from "./routes/Setup.js";
 const app = express();
 
 dotenv.config();
-app.use(express.json());
+app.use(express.json({ limit: "80mb" }));
 app.use(cors());
 app.use(bodyParser.json({ limit: "80mb" }));
 app.use(bodyParser.urlencoded({ limit: "80mb", extended: true }));
