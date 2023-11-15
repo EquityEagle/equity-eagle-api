8d5a5453992666474a87b66cfa87c7ebc24029e7
Rapheal Chizitere <www.rashyking05@gmail.com>
2023-11-16 00:44:23 +0100

    updated trade route

--- routes/Trade.js
+++ routes/Trade.js
@@ -3,7 +3,7 @@ import { DocTrade, editDoc, editPrice } from "../controller/Trade.js";
 
 const router = express.Router();
 
-router.post("/new", DocTrade);
+router.post("/new/:userId", DocTrade);
 router.patch("/edit/:docId", editDoc);
 router.patch("/edit/profit", editPrice);
 
