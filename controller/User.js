f314f6406ccd2fc75e324921de7e6614ab3c660f
Rapheal Chizitere <www.rashyking05@gmail.com>
2023-11-26 00:46:21 +0100

    updated user endpoint

--- controller/User.js
+++ controller/User.js
@@ -1,15 +1,16 @@
 import NotificationModel from "../models/Notification.js";
 import UserModel from "../models/UserModel.js";
 import cloudinary from "../utils/cloudinary.js";
+import winston from "winston";
 
 export const getUsers = async (req, res) => {
   try {
-    const users = await UserModel.find().select(
-      "-password -notification -trades"
-    );
+    const users = await UserModel.find()
+      .select("-password -notification -trades")
+      .sort({ createdAt: -1 });
     res.status(200).json(users);
   } catch (error) {
-    console.log({ error: error.message });
+    winston.error(`Error in getUsers: ${error.message}`);
     return res.status(500).json({ error: error.message });
   }
 };
