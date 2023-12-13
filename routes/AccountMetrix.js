5c407387db6b33f4fa9a519332b32721cecde2ae
Rapheal Chizitere <www.rashyking05@gmail.com>
2023-12-13 01:16:58 +0100

    updated track endpoint

--- routes/AccountMetrix.js
+++ routes/AccountMetrix.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   FindAccount,
   TrackAccount,
+  deleteAccount,
   findAccount,
   getProfitdata,
   getTrades,
@@ -14,5 +15,6 @@ router.get("/find/:trackId/one", findAccount);
 router.get("/find/:userId/all", FindAccount);
 router.get("/find/:metrixId/one/trades", getTrades);
 router.get("/find/:metrixId/one/p/data", getProfitdata);
+router.delete("/delete/:trackId", deleteAccount);
 
 export default router;
